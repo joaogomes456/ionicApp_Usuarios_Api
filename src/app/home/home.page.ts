c3af4d7865947e5cafe90ba1ea4f3e0b835344f3
JoãoGomes456 <pontesjoao80@hotmail.com>
2022-09-29 12:14:42 -0300

    29/09/2022
    Preparação para interpolação finalizada;
    
    Estilo da pagina home não iniciado.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 
@@ -8,10 +9,23 @@ import { ApiService } from '../services/api.service';
 })
 export class HomePage {
 
-  constructor(private api: ApiService) {}
+  page;
+  perPages;
+  total;
+  totalPage;
+  data;
+
+  constructor(private api: ApiService) {this.buscarTodos();}
 
   buscarTodos(page=1) {
-    this.api.buscarUsuarios(page).subscribe(usuarios=>{});
+    this.api.buscarUsuarios(page).subscribe(usuarios=>{
+      console.log(usuarios);
+      this.page = usuarios['page'];
+      this.perPages = usuarios['per_Pages'];
+      this.total = usuarios['total'];
+      this.totalPage = usuarios['totalPage'];
+      this.data = usuarios['data'];
+    });
   }
 
 }
